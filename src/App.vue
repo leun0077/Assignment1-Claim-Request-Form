a8e561d4f5a7cbd3aac1e8035f967c5cae754f65
Gabriel Leung <leun0077@algonquinlive.com>
2019-01-30 21:44:25 -0500

    installed-vue

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <PlayGround/>
+  </div>
+</template>
+
+<script>
+import PlayGround from "./components/PlayGround/PlayGround.vue";
+
+export default {
+  name: "app",
+  components: {
+    PlayGround
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/PlayGround/PlayGround.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hello">
+    <!-- Element UI -->
+    <el-input placeholder="Please input" v-model="nameInput"></el-input>
+    <!-- Watch -->
+    <input type="text" name="firstName" v-model="nameInput">
+    <!-- Computed -->
+    <div>{{message}} => Reversed: {{ reversedMessage}}</div>
+    <!-- V-Model -->
+    <label for="firstName">First Name</label>
+    <input type="text" name="firstName" v-model="nameInput">
+    <!-- V-html -->
+    <div v-html="anchorTag"></div>
+    <!-- v-once -->
+    <div v-once>The first message will never change: {{ message }}</div>
+    <div>The first message will never change: {{ message }}</div>
+    <!-- V-on -->
+    <button v-on:click="handleClick()">Click This</button>
+    <!-- V-Bind Shorthand -->
+    <div v-bind:class="backgroundClassName">Some Class with background</div>
+  </div>
+</template>
+<script src = "./PlayGround.js"></script>
+<style src = "./PlayGround.css" scoped></style>
